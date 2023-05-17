18f8954ee74bf4af72637ba86310b51310f13707
Prashant Wadekar <94431847+WadekarPrashant@users.noreply.github.com>
2023-05-17 12:35:28 +0530

    Update Problem2130.cpp

--- Problem2130.cpp
+++ Problem2130.cpp
@@ -1,24 +1,38 @@
 class Solution {
  public:
-  int rob(vector<int>& nums) {
-    if (nums.empty())
-      return 0;
-    if (nums.size() == 1)
-      return nums[0];
+  int pairSum(ListNode* head) {
+    int ans = 0;
+    ListNode* slow = head;
+    ListNode* fast = head;
 
-    auto rob = [&](int l, int r) {
-      int prev1 = 0;  // dp[i - 1]
-      int prev2 = 0;  // dp[i - 2]
+    // Let slow point to the start of the second half
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
 
-      for (int i = l; i <= r; ++i) {
-        const int dp = max(prev1, prev2 + nums[i]);
-        prev2 = prev1;
-        prev1 = dp;
-      }
+    // Tail points to the end with reversed second half
+    ListNode* tail = reverseList(slow);
 
-      return prev1;
-    };
+    while (tail) {
+      ans = max(ans, head->val + tail->val);
+      head = head->next;
+      tail = tail->next;
+    }
 
-    return max(rob(0, nums.size() - 2), rob(1, nums.size() - 1));
+    return ans;
+  }
+
+ private:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    while (head) {
+      auto next = head->next;
+      head->next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
   }
 };
+
