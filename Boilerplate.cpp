fe420b54428e95546a6c6574ccc90b16f7dc7525
WadekarPrashant <wadekarprashant733@gmail.com>
2023-06-13 11:54:32 +0530

    boilerplate

---
+++ Boilerplate.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
+#include<string.h>
+#include<math.h>
+#include<unordered_map>
+using namespace std;
+
+int main()
+{
+    /* vector<string> name(vector<int>& arr)
+    {
+    int n = arr.size();
+    } 	*/
+    
+    return 0;
+} 
+    
+
+    
