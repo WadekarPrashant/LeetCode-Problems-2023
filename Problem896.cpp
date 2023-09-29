8340b38bcef90ea96036fb6f5b80199d7cc7b10b
Prashant Wadekar <94431847+WadekarPrashant@users.noreply.github.com>
2023-09-29 23:21:53 +0530

    Problem896.cpp

---
+++ Problem896.cpp
@@ -0,0 +1,1 @@
+class Solution { public: bool isMonotonic(vector<int>& nums) { bool greater=0, less=0; int n=nums.size(); for(int i=0; i<n-1; i++){ int d=nums[i+1]-nums[i]; if (d>0) greater=1; else if (d<0) less=1; if (greater && less) return 0; } return 1; } };
